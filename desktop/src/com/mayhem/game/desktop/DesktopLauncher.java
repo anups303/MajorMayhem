a492a3e7072252b4057e5085126621764a70a6d1
dreadnought303 <kikazz303@gmail.com>
2014-05-15 12:02:54 +0200

    V1.0
    First working version - movement, camera tracking, basic collision
    detection (with arraylist coordinates!) implemented

---
+++ desktop/src/com/mayhem/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mayhem.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mayhem.game.Bomber;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bomber(), config);
+	}
+}
