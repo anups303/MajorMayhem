a01ae13c623d799fa12c4bd98e5edbc82a233797
dreadnought303 <kikazz303@gmail.com>
2014-05-29 10:58:47 +0200

    Rewritten code

--- core/src/com/mayhem/game/Bomber.java
+++ core/src/com/mayhem/game/Bomber.java
@@ -1,282 +1,268 @@
 package com.mayhem.game;
 
-import java.util.ArrayList;
-
+//for sprite
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapProperties;
+
+//for input
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+
+//for camera
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+//for map
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-//import com.badlogic.gdx.maps.tiled.TiledMapTile;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-//import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.files.*;
 
-//import com.badlogic.gdx.math.Rectangle;
-//import com.badlogic.gdx.utils.viewport.FitViewport;
-import javax.swing.JOptionPane;
+//for collision detection
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+
+
+//for randomization
 import java.util.*;
 
+//for debug
+import javax.swing.JOptionPane;
+
 public class Bomber extends ApplicationAdapter implements InputProcessor {
-	Texture img;
-	TiledMap tiledMap;
-	OrthographicCamera camera;
-    TiledMapRenderer tiledMapRenderer;
-    SpriteBatch sb;
-    Texture texture;
-    Texture txtrBomb;
-    Sprite sprite;
-    Sprite sprBomb;
-    float posX, posY;
-    //Rectangle glViewport;
-    TiledMapTileLayer collisionLayer;
-    MapProperties prop;
-    int mapWidth, mapHeight, tilePixelWidth, tilePixelHeight, mapPixelWidth, mapPixelHeight;
-    float tileWidth, tileHeight;
-    boolean collisionX = false, collisionY = false;
-    String points, line, xypt[];
-    String pattern = "(\\d+)([\\,])(\\d+)";
-    MyCoord coords;
-    ArrayList<MyCoord> coordlist;
-    //ArrayList<MyCoord> coordcheck;
+	//for sprite
+	private SpriteBatch batch;
+    private Texture texture, bombTex;
+    private Sprite sprite, bombSprite;
+    
+    //for input
+    private float posX, posY;
+    
+    //for camera
+    private OrthographicCamera camera;
+    
+    //for map
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+    
+    //for randomization
+    private Random rand = new Random();
+    
+    //for collision detection
+    private TiledMapTileLayer collisionLayer;
+    
+    private float dt=0;
+    private float elapsedTime=0;
+    private float bombX, bombY;
 	
     @Override
 	public void create () {
-		float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, w, h);						//visible game world
-        camera.update();
-        //camera.position.set(w/2, h/2, 0);					//centered sprite by commenting this out!!
-        //glViewport = new Rectangle(0, 0, w, h);
-        
-        //coords = new MyCoord();								//adding list of blocks
-        coordlist = new ArrayList<Bomber.MyCoord>();
-        FileHandle handle = Gdx.files.internal("BlockPoints.txt");
-        points = handle.readString();
-        Scanner scanner = new Scanner(points);
-        while(scanner.hasNextLine()) {
-        	coords = new MyCoord();
-        	String line = scanner.nextLine();
-        	xypt = line.split(",");
-        	coords.setX(Integer.parseInt(xypt[0]));
-        	coords.setY(Integer.parseInt(xypt[1]));
-        	coordlist.add(coords);
-        }
-        scanner.close();
-        //List<Point> points = new ArrayList<Point>();
-        
-        
-        tiledMap = new TmxMapLoader().load("MapOne.tmx");					//Map created using Tiled;present in assets
-        prop = tiledMap.getProperties();
-        mapWidth = prop.get("width", Integer.class);
-        mapHeight = prop.get("height", Integer.class);
-        tilePixelWidth = prop.get("tilewidth", Integer.class);
-        tilePixelHeight = prop.get("tileheight", Integer.class);
-        mapPixelWidth = mapWidth * tilePixelWidth;
-        mapPixelHeight = mapHeight * tilePixelHeight;
+    	//for map
+        tiledMap = new TmxMapLoader().load("MapTwo.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        collisionLayer = new TiledMapTileLayer(mapWidth, mapHeight, tilePixelWidth, tilePixelHeight);
         
-        tileWidth = collisionLayer.getTileWidth();
-        tileHeight = collisionLayer.getTileHeight();
+        //for collision detection
+        collisionLayer = (TiledMapTileLayer)tiledMap.getLayers().get("Fore");
+    	
+		//for sprite
+    	batch = new SpriteBatch();
+        texture = new Texture(Gdx.files.internal("Bman_f_f00.png"));
+        sprite = new Sprite(texture);
+        bombTex = new Texture(Gdx.files.internal("Bomb_f01.png"));
+        bombSprite = new Sprite(bombTex);
+        bombSprite.setSize(32, 32);
+        
+        //for sprite position
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        sprite.setSize(32.0f, 64.0f);
+        posX = 32*(rand.nextInt(29)+1);
+        posY = 32*(rand.nextInt(29)+1);
+        //check whether overlapping with another block
+        while(collisionLayer.getCell((int)posX/32,(int)posY/32) != null) {
+        	posX = 32*(rand.nextInt(29)+1);
+            posY = 32*(rand.nextInt(29)+1);
+        }
+        sprite.setPosition(posX,posY);
+        bombSprite.setPosition(posX, posY);
         Gdx.input.setInputProcessor(this);
         
-        sb = new SpriteBatch();
-        texture = new Texture(Gdx.files.internal("Bman_f_f00.png"));		//Character sprite;present in assets
-        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-        sprite = new Sprite(texture);
-        
-        txtrBomb = new Texture(Gdx.files.internal("Bomb_f01.png"));
-        txtrBomb.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-        sprBomb = new Sprite(txtrBomb);
-        sprBomb.setSize(32, 32);
-        //sprite.setOrigin(50, 50);
-        sprite.setSize(32, 64);
-        //posX = 320.0f;
-        posX = 320.0f;
-        //posY = 2944.0f;
-        posY = 2944.0f;
-        sprite.setPosition(posX, posY);
-        //sprBomb.setPosition(posX, posY);
-        camera.position.x = posX;
-        camera.position.y = posY;
+        //for camera
+    	camera = new OrthographicCamera(w, h);
+    	camera.setToOrtho(false);
+    	camera.position.x = posX;
+    	camera.position.y = posY;
+        camera.update();
 	}
 
 	@Override
 	public void dispose () {
-		sb.dispose();
-		texture.dispose();
-		txtrBomb.dispose();
+		//for sprite
+		batch.dispose();
+        texture.dispose();
 	}
 	
 	@Override
 	public void render () {
-		float deltaTime = Gdx.graphics.getDeltaTime();
-		Gdx.gl.glClearColor(1, 1, 1, 1);									//clear screen RGB values
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		//for sprite
+		Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
-        /*camera.position.x = sprite.getX();
-        camera.position.y = sprite.getY();*/
-        camera.update();
+        //dt = Gdx.graphics.getDeltaTime();
+        //JOptionPane.showMessageDialog(null, dt);
         
-        //if polling for KB input
-        /*if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
-        	sprite.translateX(-1f);
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-        	sprite.translateX(1f);
-        if(Gdx.input.isKeyPressed(Input.Keys.UP))
-        	sprite.translateY(1f);
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
-        	sprite.translateY(-1f);*/
+        //for input
+        sprite.setPosition(posX,posY);
+        //bombSprite.setPosition(posX, posY);
         
+        //for camera
+        batch.setProjectionMatrix(camera.combined);
+        
+        //for map
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
-        sb.setProjectionMatrix(camera.combined);
-        sprite.setPosition(posX, posY);
-        sb.begin();
-        sprite.draw(sb);
+                
+        batch.begin();
+        sprite.draw(batch);
         if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-        	sprBomb.setPosition(sprite.getX(), sprite.getY());
-			sprBomb.draw(sb);
-        	//JOptionPane.showMessageDialog(null, points);
+        	bombX = sprite.getX();
+        	bombY = sprite.getY();
+        	for(elapsedTime=0;elapsedTime<3;elapsedTime+=dt) {
+        		dt = Gdx.graphics.getDeltaTime();
+        		bombSprite.setPosition(bombX,bombY);
+        		bombSprite.draw(batch);
+        	}
         }
-        //sprBomb.draw(sb);
-        sb.end();
+        batch.end();
+        
+        /*batch.begin();
+        if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+        	bombSprite.setPosition(sprite.getX(),sprite.getY());
+        	bombSprite.draw(batch);
+        }
+        batch.end();*/
 	}
 	
 	@Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+    
+    @Override
     public boolean keyDown(int keycode) {
-		float moveAmount = 32.0f;
-		MyCoord coords1 = new MyCoord();
-		if(keycode == Keys.LEFT) {
-			/*collisionX = collisionLayer.getCell((int)(sprite.getX()/tileWidth), (int)(sprite.getY()/tileHeight))
-					.getTile().getProperties().containsKey("blocked");*/
-			//JOptionPane.showMessageDialog(null, (int)(sprite.getX()-moveAmount)+" "+(int)(sprite.getY()));
-			
-			/*Cell c = collisionLayer.getCell((int)(sprite.getX()/tileWidth),(int)(sprite.getY()/tileHeight));
-			TiledMapTile tl = c.getTile();
-			MapProperties mp = tl.getProperties();
-			if(mp.containsKey("blocked"))
-				JOptionPane.showMessageDialog(null, "blocked");*/
-			
-			coords1.setX((int)(sprite.getX()-moveAmount));
-			coords1.setY((int)(sprite.getY()));
-						
-			//JOptionPane.showMessageDialog(null, (int)(sprite.getX()-moveAmount)+"+"+(int)sprite.getY());
-			if(!coordlist.contains(coords1)) {
-				if((int)sprite.getX()>32)
-					posX -= moveAmount;
-				if((int)sprite.getX()>320 && (int)sprite.getX()<=2880)
-					camera.translate((int) -moveAmount,0,0);
-			}
-		}
-		if(keycode == Keys.RIGHT) {
-			/*collisionX = collisionLayer.getCell((int)(sprite.getX()/tileWidth), (int)(sprite.getY()/tileHeight))
-					.getTile().getProperties().containsKey("blocked");*/
-			coords1.setX((int)(sprite.getX()+moveAmount));
-			coords1.setY((int)(sprite.getY()));
-			
-			if(!coordlist.contains(coords1)) {
-				if((int)sprite.getX()<3136)
-					posX += moveAmount;
-				if((int)sprite.getX()>=320 && (int)sprite.getX()<2880)
-					camera.translate((int) moveAmount,0,0);
-			}
-		}
-		if(keycode == Keys.UP) {
-			/*collisionY = collisionLayer.getCell((int)(sprite.getX()/tileWidth), (int)(sprite.getY()/tileHeight))
-					.getTile().getProperties().containsKey("blocked");*/
-			coords1.setX((int)(sprite.getX()));
-			coords1.setY((int)(sprite.getY()+moveAmount));
-			
-			if(!coordlist.contains(coords1)) {
-				if((int)sprite.getY()<3136)
-					posY += moveAmount;
-				if((int)sprite.getY()>=256 && (int)sprite.getY()<2944)
-					camera.translate(0,(int) moveAmount,0);
-			}
-		}
-		if(keycode == Keys.DOWN) {
-			/*collisionY = collisionLayer.getCell((int)(sprite.getX()/tileWidth), (int)(sprite.getY()/tileHeight))
-					.getTile().getProperties().containsKey("blocked");*/
-			coords1.setX((int)(sprite.getX()));
-			coords1.setY((int)(sprite.getY()-moveAmount));
-			
-			if(!coordlist.contains(coords1)) {
-				if((int)sprite.getY()>32)
-					posY -= moveAmount;
-				if((int)sprite.getY()>256 && (int)sprite.getY()<=2944)
-					camera.translate(0,(int) -moveAmount,0);
-			}
-		}
-		if(keycode == Keys.SPACE) {
-			//JOptionPane.showMessageDialog(null, (int)sprite.getX()+"+"+(int)sprite.getY());
-			//JOptionPane.showMessageDialog(null, coordlist.get(0));
-			//sprBomb.setPosition(sprite.getX(), sprite.getY());
-			//sprBomb.draw(sb);
-			
-			/*coords = new MyCoord();								//adding list of blocks
-	        coordlist = new ArrayList<Bomber.MyCoord>();
-	        FileHandle handle = Gdx.files.internal("BlockPoints.txt");
-	        points = handle.readString();
-	        Scanner scanner = new Scanner(points);
-	        while(scanner.hasNextLine()) {
-	        	coords = new MyCoord();
-	        	String line = scanner.nextLine();
-	        	xypt = line.split(",");
-	        	//JOptionPane.showMessageDialog(null, Integer.parseInt(xypt[0])+" "+Integer.parseInt(xypt[1]));
-	        	coords.setX(Integer.parseInt(xypt[0]));
-	        	coords.setY(Integer.parseInt(xypt[1]));
-	        	coordlist.add(coords);
-	        }
-	        scanner.close();*/
-	        
-	        /*for(MyCoord item: coordlist)
-	        	JOptionPane.showMessageDialog(null, item.X+" "+item.Y);*/
-	        
-	        /*coords1.setX((int)posX);
-	        coords1.setY((int)posY);
-	        if(coordlist.contains(coords1))
-	        	JOptionPane.showMessageDialog(null, (int)posX+" "+(int)posY+" present");*/
-		}
+        //tile width and height set at 32 px
+    	float moveAmount = 32.0f;
+        Cell cell;
+    	
+        if(keycode == Keys.LEFT) {
+        	//convert pixel position to tile position
+        	cell = collisionLayer.getCell(((int)(posX-moveAmount))/32,(int)posY/32);
+            if(cell != null) {
+            	if(!cell.getTile().getProperties().containsKey("blocked")) {
+	            	posX-=moveAmount;
+	            	camera.translate(-moveAmount, 0);
+	            	camera.update();
+	            }
+            }
+            
+            if(cell == null) {
+            	posX-=moveAmount;
+            	camera.translate(-moveAmount, 0);
+            	camera.update();
+            }
+        }
+        if(keycode == Keys.RIGHT) {
+            cell = collisionLayer.getCell(((int)(posX+moveAmount))/32,(int)posY/32);
+            if(cell != null) {
+            	if(!cell.getTile().getProperties().containsKey("blocked")) {
+	            	posX+=moveAmount;
+	            	camera.translate(moveAmount, 0);
+	                camera.update();
+	            }
+            }
+            
+            if(cell == null) {
+            	posX+=moveAmount;
+            	camera.translate(moveAmount, 0);
+                camera.update();
+            }
+        }
+        if(keycode == Keys.DOWN) {
+        	cell = collisionLayer.getCell(((int)posX)/32,(int)(posY-moveAmount)/32);
+            if(cell != null) {
+            	if(!cell.getTile().getProperties().containsKey("blocked")) {
+	            	posY-=moveAmount;            
+	            	camera.translate(0, -moveAmount);
+	            	camera.update();
+	            }
+            }
+        	
+        	if(cell == null) {
+        		posY-=moveAmount;            
+            	camera.translate(0, -moveAmount);
+            	camera.update();
+            }
+        }
+        if(keycode == Keys.UP) {
+        	cell = collisionLayer.getCell(((int)posX)/32,(int)(posY+moveAmount)/32);
+            if(cell != null) {
+            	if(!cell.getTile().getProperties().containsKey("blocked")) {
+	            	posY+=moveAmount;        	
+	            	camera.translate(0, moveAmount);
+	            	camera.update();
+	            }
+            }
+        	
+            if(cell == null) {
+            	posY+=moveAmount;        	
+            	camera.translate(0, moveAmount);
+            	camera.update();
+            }
+        }
+        if(keycode == Keys.SPACE) {
+        	//JOptionPane.showMessageDialog(null, ((int)posX)/32+"+"+(992-(int)posY)/32);
+        }
         return true;
     }
 
     @Override
     public boolean keyUp(int keycode) {
-    	//to pan camera - commented out
-        /*if(keycode == Input.Keys.LEFT)
-            camera.translate(-32,0);
-        if(keycode == Input.Keys.RIGHT)
-            camera.translate(32,0);
-        if(keycode == Input.Keys.UP)
-            camera.translate(0,32);
-        if(keycode == Input.Keys.DOWN)
-            camera.translate(0,-32);
-        if(keycode == Input.Keys.NUM_1)
-            tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
-        if(keycode == Input.Keys.NUM_2)
-            tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());*/
         return false;
     }
 
     @Override
     public boolean keyTyped(char character) {
+        return false;
+    }
+    
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
 
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
@@ -284,65 +270,5 @@ public class Bomber extends ApplicationAdapter implements InputProcessor {
     public boolean scrolled(int amount) {
         return false;
     }
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	static class MyCoord{
-	    private int X;
-	    private int Y;
-
-	    public MyCoord() {
-	        this(0,0);
-	    }        
-	    public MyCoord(int X, int Y) {
-	        this.X = X;
-	        this.Y = Y;
-	    }
-	    public int getX() {
-	        return X;
-	    }
-	    public int getY() {
-	        return Y;
-	    }
-	    public void setX(int X) {
-	        this.X = X;
-	    }
-	    public void setY(int Y) {
-	        this.Y = Y;
-	    }
-	    
-	    @Override
-	    public boolean equals(Object obj) {
-	    	if(obj == this)
-	    		return true;
-	    	if(obj == null || obj.getClass() != this.getClass())
-	    		return false;
-	    	MyCoord coord1 = (MyCoord) obj;
-	    	return X == coord1.X && Y == coord1.Y;
-	    }
-	    
-	}
 }
 
--- desktop/src/com/mayhem/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mayhem/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mayhem.game.Bomber;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "MajorMayhem";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new Bomber(), config);
 	}
 }
